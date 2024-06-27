e0b46c2c75a77134d36486a6471095b75228e749
39sho <g.39sho@gmail.com>
2024-06-27 19:22:16 +0900

    デバッグ用のメッセージを削除

--- packages/server/server/routes/_ws.ts
+++ packages/server/server/routes/_ws.ts
@@ -5,7 +5,9 @@ const rooms = new WeakMap<Peer, string>();
 export default defineWebSocketHandler({
     open(peer) {
         console.log("[ws] open", peer);
+        /*
         peer.send(`[system] you are ${peer}`);
+        */
     },
 
     message(peer, message) {
@@ -14,10 +16,12 @@ export default defineWebSocketHandler({
             rooms.set(peer, name);
 
             peer.subscribe(rooms.get(peer) ?? "test-room");
+            /*
             peer.publish(
                 rooms.get(peer) ?? "test-room",
                 `[system] ${peer} joined the chat!`
             );
+            */
 
             return;
         }
@@ -29,10 +33,12 @@ export default defineWebSocketHandler({
 
     close(peer, event) {
         console.log("[ws] close", peer, event);
+        /*
         peer.publish(
             rooms.get(peer) ?? "test-room",
             `[system] ${peer} has left the chat!`
         );
+        */
         peer.unsubscribe(rooms.get(peer) ?? "test-room");
     },
 
